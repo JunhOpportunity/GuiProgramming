8eafaad8260995d0a5c9a390404ecebae3ec2782
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-09-02 22:16:35 +0900

    Create Gui Frame

---
+++ 1_create_gui_frame.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+root = Tk()
+root.title("New page")      # 창 제목
+# root.geometry("640x480")    # 가로 * 세로
+root.geometry("640x480+300+100")    # 가로*세로+x좌표+y좌표 (※사이 사이 띄어쓰기 금지!!)
+root.mainloop()

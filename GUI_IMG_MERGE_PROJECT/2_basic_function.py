97cf2ca9cc097af61462de5f308f6e64c6c57e73
JunHo <89464762+JunhOpportunity@users.noreply.github.com>
2021-09-07 22:36:05 +0900

    missed progress bar

--- GUI_IMG_MERGE_PROJECT/2_basic_function.py
+++ GUI_IMG_MERGE_PROJECT/2_basic_function.py
@@ -1,4 +1,5 @@
 import tkinter.ttk as ttk
+import tkinter.messagebox as msgbox
 from tkinter import *
 from tkinter import filedialog
 
@@ -17,7 +18,34 @@ def add_file():
 
 # 파일 제거 함수
 def del_file():
-    pass
+    for index in reversed(list_file.curselection()):
+        list_file.delete(index)
+
+# 저장 경로(folder)
+def browse_dest_path():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected == '':   # 사용자가 취소 누른 경우
+        return
+    txt_dest_path.delete(0, END)
+    txt_dest_path.insert(0, folder_selected)
+
+
+# 시작
+def start():
+    # 각 옵션들 값 확인하기
+    print("가로넓이 : ", comb_width.get())
+    print("간격 : ", comb_space.get())
+    print("포맷 : ", comb_format.get())
+
+    # 파일 목록 확인
+    if list_file.size() == 0:
+        msgbox.showwarning("경고", "이미지 파일을 추가하세요")
+        return
+    
+    # 저장 경로 확인
+    if len(txt_dest_path.get()) == 0:
+        msgbox.showwarning("경고", "저장 경로를 선택하세요")
+
 
 # 파일 프레임 (파일 추가, 선택 삭제)
 file_frame = Frame(root)
@@ -57,7 +85,7 @@ path_frame.pack(fill="x", padx=5, pady=5)
 txt_dest_path = Entry(path_frame)
 txt_dest_path.pack(side="left", fill="x", expand=True, ipady=4) # ipad는 내부 높이 변경
 
-btn_dest_path = Button(path_frame, text="찾아보기", width=10)
+btn_dest_path = Button(path_frame, text="찾아보기", width=10, command=browse_dest_path)
 btn_dest_path.pack(side="right", padx=5, pady=5)
 
 # 옵션 프레임
@@ -101,6 +129,7 @@ comb_format.pack(side="left", padx=5, pady=5)
 frame_progress = LabelFrame(root, text="진행상황")
 frame_progress.pack(fill="x", padx=5, pady=5)
 
+
 p_var = DoubleVar()
 progress_bar = ttk.Progressbar(frame_progress, maximum=100, variable=p_var)
 progress_bar.pack(fill="x", padx=5, pady=5)
@@ -112,7 +141,7 @@ frame_run.pack(fill="x", padx=5, pady=5)
 btn_close = Button(frame_run, padx=5, pady=5, text="닫기", width=12, command=root.quit)
 btn_close.pack(side="right", padx=5, pady=5)
 
-btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12)
+btn_start = Button(frame_run, padx=5, pady=5, text="시작", width=12, command=start)
 btn_start.pack(side="right", padx=5, pady=5)
 
 
